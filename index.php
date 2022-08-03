533caddc79fc7af2e86a5923090e5ef4db7a4280
krd <107356857+bazo899@users.noreply.github.com>
2022-08-03 13:06:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+if(isset($_POST['name']))
+    {
+    echo $_POST['login'] . "<br>";
+
+
+}
+
+echo $_SERVER['HTTP_USER_AGENT'];
+
+?>
+
+
+<form method="POST">
+    
+    <input name="login">
+    <input type="submit" name="name">
+   
+   
+    
+</form>
